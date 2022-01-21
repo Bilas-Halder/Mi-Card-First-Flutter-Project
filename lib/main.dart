95667e7c6b4a82e354f4eb5eefa3e074d29c1eb4
Bilas-Halder <bilas8642@gmail.com>
2022-01-21 16:59:31 +0600

    Added Row Collumn Layout & Metarial Icons & Card

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,60 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold
                   ),
                 ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 17,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: 130,
+                  height: 3,
+                  child: Container(
+                    color: Colors.teal[300],
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone_in_talk,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      "+880 1700 694 152",
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(horizontal: 50, vertical: 0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      "bilas8642@gmail.com",
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           )
